54cf1a62b58f9925e478909edad986161a3f24db
PaJlma <palma21042005@gmail.com>
2023-12-04 18:40:51 +0300

    configured omnisharp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "omnisharp" }
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,5 +13,10 @@ for _, lsp in ipairs(servers) do
   }
 end
 
---
--- lspconfig.pyright.setup { blabla}
+lspconfig.omnisharp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "omnisharp.cmd", "--languageserver" },
+}
+
+-- lspconfig.pyright.setup { blablai }
